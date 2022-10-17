fc5324c9db281afbbb84cd6df7051c510b8f93a5
Delyc <d.twizeyima@alustudent.com>
2022-10-17 17:26:15 +0200

    mobile and desktop version

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "940px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "640px" },
+      // => @media (max-width: 639px) { ... }
+    },
+  },
+  plugins: [],
+}
